185521f9abba25b0fe52f573fe147265a948e597
Kevin Cóndor <condorsua@hotmail.com>
2022-05-13 07:12:20 -0500

    Deber

--- Parcial 1/Deberes/IngresoFloat/main.cpp
+++ Parcial 1/Deberes/IngresoFloat/main.cpp
@@ -1,3 +1,12 @@
+/*Universidad de las Fuerzas Armadas ESPE
+Título: Funcion Ingresar
+#Grupo
+Autor/es: Kevin Condor, Christopher Diaz
+Fecha de creacíon: 11/05/2022
+Fecha de modificación: 11/05/2022
+Ingeniería en Software
+Estructura de Datos*/
+
 #include <iostream>
 #include <conio.h>
 #include <stdio.h>
--- Parcial 1/Deberes/SChar/Aplicativo.cpp
+++ Parcial 1/Deberes/SChar/Aplicativo.cpp
@@ -1,3 +1,12 @@
+/*Universidad de las Fuerzas Armadas ESPE
+Título: Funcion Ingresar
+#Grupo
+Autor/es: Kevin Condor, Christopher Diaz
+Fecha de creacíon: 12/05/2022
+Fecha de modificación: 12/05/2022
+Ingeniería en Software
+Estructura de Datos*/
+
 #include <iostream>
 #include <conio.h>
 #include <stdio.h>
